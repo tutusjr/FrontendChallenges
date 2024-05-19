3636ebf19fca81a7687a36069a2290d891980bea
Mustafa_Tutus <tutusfullstack@gmail.com>
2024-05-19 21:02:19 +0300

    complete

--- newsletter/script.js
+++ newsletter/script.js
@@ -11,33 +11,39 @@ const dismissButton = document.getElementById('dismiss-button');
 
 var regexForEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.(com|org|net|edu|gov|mil|biz|info|mobi|name|aero|asia|jobs|museum)$/;
 
-submitButton.addEventListener('click', (event) => {
+const handleSubmit = (event) => {
     event.preventDefault();
     const email = emailInput.value;
     if (!email || !regexForEmail.test(email)) {
         emailInput.classList.add('error');
         errorMessage.classList.add('show-error');
         emailInput.placeholder = 'ash#loremcompany.com';
-    }else {
+    } else {
         emailInput.classList.remove('error');
         errorMessage.classList.remove('show-error');
         successScreen.classList.add('success-visible');
         container.classList.add('show');
-        for (const element of content){
+        for (const element of content) {
             element.classList.add('hide');
         }
-        successMessage.innerHTML = `A confirmation email has been sent to <b>${email}</b> Please open it and click the button inside to confirm your subscription.`
+        successMessage.innerHTML = `A confirmation email has been sent to <b>${email}</b>. Please open it and click the button inside to confirm your subscription.`;
+    }
+};
+
+submitButton.addEventListener('click', handleSubmit);
+
+document.addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
+        handleSubmit(event);
     }
 });
 
 dismissButton.addEventListener('click', (e) => {
     e.preventDefault();
-    const email = emailInput.value;
     successScreen.classList.remove('success-visible');
     container.classList.remove('show');
     emailInput.value = '';
-    for (const element of content){
+    for (const element of content) {
         element.classList.remove('hide');
     }
 });
-
