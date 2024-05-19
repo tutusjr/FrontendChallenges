f6e9a7cac52e45ef678c20533dd2f0c6c47e5a4d
Mustafa_Tutus <tutusfullstack@gmail.com>
2024-05-19 20:18:49 +0300

    complete project

---
+++ newsletter/script.js
@@ -0,0 +1,43 @@
+// script.js dosyası
+
+const submitButton = document.getElementById('submit-button');
+const emailInput = document.getElementById('email');
+const errorMessage = document.getElementById('error-message');
+const successMessage = document.getElementById('success-message');
+const successScreen = document.getElementById('success');
+const content = document.getElementsByClassName('content');
+const container = document.getElementById('container');
+const dismissButton = document.getElementById('dismiss-button');
+
+var regexForEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.(com|org|net|edu|gov|mil|biz|info|mobi|name|aero|asia|jobs|museum)$/;
+
+submitButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    const email = emailInput.value;
+    if (!email || !regexForEmail.test(email)) {
+        emailInput.classList.add('error');
+        errorMessage.classList.add('show-error');
+        emailInput.placeholder = 'ash#loremcompany.com';
+    }else {
+        emailInput.classList.remove('error');
+        errorMessage.classList.remove('show-error');
+        successScreen.classList.add('success-visible');
+        container.classList.add('show');
+        for (const element of content){
+            element.classList.add('hide');
+        }
+        successMessage.innerHTML = `A confirmation email has been sent to <b>${email}</b> Please open it and click the button inside to confirm your subscription.`
+    }
+});
+
+dismissButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    const email = emailInput.value;
+    successScreen.classList.remove('success-visible');
+    container.classList.remove('show');
+    emailInput.value = '';
+    for (const element of content){
+        element.classList.remove('hide');
+    }
+});
+
